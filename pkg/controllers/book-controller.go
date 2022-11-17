1efa335226ed18e7033008d281edbdf12937ff9c
Alex <AlexGrimes.se@gmail.com>
2022-11-17 17:40:18 -0500

    added Delete book function

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"go-bookstore/pkg/models"
+	"go-bookstore/pkg/utils"
 	"net/http"
 	"strconv"
 
@@ -34,4 +35,25 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 	w.Write(res)
 }
 
-// TODO - create delete and update methods
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
